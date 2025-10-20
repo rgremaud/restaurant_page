af9c4bf35e090ea35da823c753247b9191a31a8a
rgremaud <rgremau@gmail.com>
2025-10-19 21:31:44 -0400

    Add some initial menu items

--- src/home.js
+++ src/home.js
@@ -5,6 +5,9 @@ function createHomeDiv () {
     homeDiv.id = "homeDiv";
 
     createTitle(homeDiv);
+    insertImage(homeDiv);
+    createInfo(homeDiv);
+    createHours(homeDiv);
 
     return homeDiv;
 }
@@ -16,5 +19,35 @@ function createTitle (parentDiv) {
 
     parentDiv.appendChild(title);
 }
+
+function insertImage (parentDiv) {
+    const image = document.createElement("div");
+    image.id = "image";
+    image.textContent = "Place holder for homepage image";
+
+    parentDiv.appendChild(image);
+}
+
+function createInfo (parentDiv) {
+    const info = document.createElement("p");
+    info.id = "info";
+    info.textContent = `Most people don't think a 3 year old can create delicious icecream.\n  Come try homemade creations from our young prodigy.`
+
+    parentDiv.appendChild(info);
+}
+
+function createHours (parentDiv) {
+    const topLine = document.createElement("h2");
+    topLine.id = "topLine";
+    topLine.textContent = "Open Daily"
+
+    const bottomLine = document.createElement("div");
+    bottomLine.id = "bottomLine";
+    bottomLine.textContent = "11AM to 9PM"
+
+    parentDiv.appendChild(topLine);
+    parentDiv.appendChild(bottomLine);
+}
+
 
 export { createHomeDiv };
--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,7 @@ function createMenuDiv () {
     menuDiv.id = "menuDiv";
 
     createTitle(menuDiv);
+    createMenuDetails(menuDiv);
 
     return menuDiv;
 }
@@ -15,6 +16,25 @@ function createTitle (parentDiv) {
     title.textContent = "Theo's Menu"
 
     parentDiv.appendChild(title);
+}
+
+function createMenuDetails (parentDiv) {
+    // Specials - $15
+    const specials = document.createElement("h3");
+    specials.textContent = "Specials - $15";
+    // The Kitchen Sink - Strawberry, Mint-Chocolate Chip, Coffee, Cookie-Dough ice cream topped with sprinkles
+    // Sprinkles Rule - Choice of any flavor topped with two servings of sprinkles
+    // Dealer's Choice - Three random flavors selected by our ice cream wizard
+
+    // Single Scoop - $5 
+    // Double Scoop - $8
+
+    // Flavors
+    // Vanilla Chocolate Strawbery  
+    // Mint Chocolate Chip Rocky Road Cookie Dough
+    // Coffee Peach Cobbler Cookies-n-Cream
+
+    parentDiv.appendChild(specials);
 }
 
 export { createMenuDiv };
